98d9fdb562286a9f25f968d9613f669e5fc3d4b9
Andres Orozco <ao@macbook-pro.attlocal.net>
2018-06-24 08:28:47 -0400

    add main.go to view websocket data

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/andresoro/hft/clients/gdax"
+)
+
+func main() {
+	// init client
+	client := gdax.NewClient("test", "test", "test")
+	products := []string{"ETH-USD"}
+
+	// connect to WS with desired product
+	client.Connect()
+	client.Subscribe(products)
+
+	// infinite for loop which reads incoming messages
+	var msg gdax.Message
+	for true {
+
+		if err := client.WSConn.ReadJSON(&msg); err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+		fmt.Println(msg.Side, msg.Price)
+
+	}
+}
